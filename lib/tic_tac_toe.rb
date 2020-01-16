47560e404fb8222b4037f216f7f2e9f337f5d435
Anthony Pardo <pardo.anthony@gmail.com>
2020-01-16 18:17:53 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -79,7 +79,7 @@ class TicTacToe
   end
   
   def won?
-    WIN_COMBINATIONS.detect do |combo| (position_taken?(combo[0]) && @board[combo[0]] == @board[combo[1]]
+    WIN_COMBINATIONS.detect do |combo| (position_taken?(combo[0]) && (@board[combo[0]] == @board[combo[1]]
       && @board[combo[1]] == @board[combo[2]])
     end
   end
