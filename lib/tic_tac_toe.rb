d198a92d2eacf9661cb03a50635483244eb3a70c
Anthony Pardo <pardo.anthony@gmail.com>
2020-01-16 18:54:44 +0000

    Done.

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,130 @@
+class TicTacToe 
+  WIN_COMBINATIONS = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+    ]
+  def initialize
+    @board = [" "," "," ",
+              " "," "," ",
+              " "," "," "]
+  end
+  
+  def display_board
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts "-----------"
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts "-----------"
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end
+  
+  def input_to_index(input) 
+    input.to_i - 1
+  end
+  
+  def move(index, token = "X")
+    @board[index] = token
+  end
+  
+  def position_taken?(index)
+    if @board[index] == "X" || @board[index] == "O"
+      return true 
+    else 
+      return false 
+    end
+  end
+  
+  def valid_move?(position)
+    if @board[position] == " " && position.between?(0, 8)
+      return true 
+    else 
+      return false 
+    end
+  end
+  
+  def turn_count
+    count = 0
+    @board.each do |position|
+      if position == "X" || position == "O"
+        count += 1 
+      end
+    end
+    count
+  end
+  
+  def current_player
+    if turn_count % 2 == 0 
+      return "X"
+    else 
+      return "O"
+    end 
+  end
+  
+  def turn 
+    puts "Please enter a move:"
+    position = gets.chomp
+    index = input_to_index(position)
+    marker = current_player
+    if valid_move?(index)
+       move(index, marker)
+       display_board
+    else 
+      turn
+    end
+  end
+  
+  def won?
+    WIN_COMBINATIONS.detect { |combo| (@board[combo[0]] == "X" || @board[combo[1]] == "O") && (@board[combo[0]] == @board[combo[1]]) && (@board[combo[1]] == @board[combo[2]]) }
+  end
+  
+  def full?
+    flag = true
+    @board.each do |element|
+      if element == "X" || element == "O"
+      else 
+        flag = false 
+      end
+    end
+    flag
+  end
+  
+  def draw? 
+    if won? 
+      return false 
+    elsif full? && !won? 
+      return true 
+    else 
+      return false 
+    end
+  end
+  
+  def over? 
+    if won? || full? 
+      return true 
+    else 
+      return false 
+    end
+  end
+  
+  def winner 
+    if won? 
+      return @board[won?[0]]
+    end
+  end
+  
+  def play 
+    turn until over? || draw?
+
+    if won? 
+      puts "Congratulations #{winner}!"
+    elsif draw? 
+      puts "Cat's Game!" 
+    end
+      
+  end
+end
